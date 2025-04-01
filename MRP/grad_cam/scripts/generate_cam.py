cb6855a125a8159166955dc0dcb2127a967f9785
nickhezy <nickzyhe@gmail.com>
2025-04-01 13:48:09 +0800

    feat: generate pseudo masks from supressCAM and compute mIoU

--- MRP/grad_cam/scripts/generate_cam.py
+++ MRP/grad_cam/scripts/generate_cam.py
@@ -39,79 +39,81 @@ def overlay_image_and_heatmap(image, heatmap, alpha=0.5):
     heatmap = heatmap.resize(image.size)
     return Image.blend(image.convert("RGB"), heatmap.convert("RGB"), alpha)
 
-# === Create output dir
-os.makedirs(CAM_DIR, exist_ok=True)
 
-# === Preprocessing
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406],
-                         [0.229, 0.224, 0.225])
-])
+if __name__ == '__main__':
+    # === Create output dir
+    os.makedirs(CAM_DIR, exist_ok=True)
 
-# === Load one image
-dataset = PetClassificationDataset(IMAGE_DIR, TRAIN_FILE, transform=transform)
-print("len(dataset): ", len(dataset))
+    # === Preprocessing
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                            [0.229, 0.224, 0.225])
+    ])
 
-for i in range(10):
-    model_name = "mobilenet" # "resnet"
-    image_tensor, label, image_name = dataset[i*100 + 1]
-    # print("label: ", label)
+    # === Load one image
+    dataset = PetClassificationDataset(IMAGE_DIR, TRAIN_FILE, transform=transform)
+    print("len(dataset): ", len(dataset))
 
-    # === Load model
-    if model_name == "resnet":
-        model = get_resnet18(num_classes=37)
-        state_dict = torch.load(os.path.join(CHECKPOINT_DIR, 'resnet18_cls_epoch5.pth'), map_location='cpu')
-    elif model_name == "mobilenet":
-        model = get_mobilenet_v3_small()
-        state_dict = torch.load(os.path.join(CHECKPOINT_DIR, 'mobilenet_epoch9.pth'), map_location='cpu')
-    else:
-        raise ValueError(f"Invalid model name: {model_name}")
-    
-    
-    model.load_state_dict(state_dict)
+    for i in range(10):
+        model_name = "mobilenet" # "resnet"
+        image_tensor, label, image_name = dataset[i*100 + 1]
+        # print("label: ", label)
 
-    generate_cam_fn = generate_grad_cam_mobilenet
-    # generate_cam_fn = generate_grad_cam
-
-
-
-    # choose chose a few false classes (included_classes) to to negative grad-CAM
-    # when included_classes is empty, algorithm reduces to normal grad-CAM
-
-    excluded_classes = [i for i in range(0,37)]
-    # included_classes = [0,2,7,16,19,37]
-    # included_classes = [0,2,7,16,20,21,37]
-    included_classes = []
-    excluded_classes = list(set(excluded_classes) - set(included_classes))
+        # === Load model
+        if model_name == "resnet":
+            generate_cam_fn = generate_grad_cam
+            model = get_resnet18(num_classes=37)
+            state_dict = torch.load(os.path.join(CHECKPOINT_DIR, 'resnet18_cls_epoch5.pth'), map_location='cpu')
+        elif model_name == "mobilenet":
+            generate_cam_fn = generate_grad_cam_mobilenet
+            model = get_mobilenet_v3_small()
+            state_dict = torch.load(os.path.join(CHECKPOINT_DIR, 'mobilenet_epoch9.pth'), map_location='cpu')
+        else:
+            raise ValueError(f"Invalid model name: {model_name}")
+        
+        
+        model.load_state_dict(state_dict)
 
 
 
 
-    # === Generate CAM
-    grad_cam_list = [
-        # negative grad-CAM for false classes
-        generate_cam_fn(model, image_tensor, target_class=c, negative=True)
-        for c in range(0, 37) if (c not in excluded_classes) and (c != label)
-    ]
-    # positive grad-CAM for the true labelled class 
-    grad_cam_list.append(generate_cam_fn(model, image_tensor, target_class=label, negative=False))
-    grad_cam_maxed = np.maximum.reduce(grad_cam_list)
+        # choose chose a few false classes (included_classes) to to negative grad-CAM
+        # when included_classes is empty, algorithm reduces to normal grad-CAM
+
+        excluded_classes = [i for i in range(0,37)]
+        # included_classes = [0,2,7,16,19,37]
+        included_classes = [0,2,7,16,20,21,37]
+        # included_classes = []
+        excluded_classes = list(set(excluded_classes) - set(included_classes))
 
 
-    # === Convert everything
-    original_image = denormalize_image(image_tensor)
-    heatmap = cam_to_heatmap(grad_cam_maxed)
-    overlay = overlay_image_and_heatmap(original_image, heatmap, alpha=0.5)
 
-    # # === Save original image
-    # save_path = os.path.join(CAM_DIR, f'{image_name}.jpg')
-    # original_image.save(save_path)
 
-    # === Save overlay
-    save_path = os.path.join(CAM_DIR, f'{image_name}_neggradcam_overlay_mbn.jpg')
-    if not included_classes:
-        save_path = os.path.join(CAM_DIR, f'{image_name}_gradcam_overlay_mbn.jpg')
-    overlay.save(save_path)
-    print(f"✅ neg-grad-CAM overlay img saved to: {save_path}")
+        # === Generate CAM
+        grad_cam_list = [
+            # negative grad-CAM for false classes
+            generate_cam_fn(model, image_tensor, target_class=c, negative=True)
+            for c in range(0, 37) if (c not in excluded_classes) and (c != label)
+        ]
+        # positive grad-CAM for the true labelled class 
+        grad_cam_list.append(generate_cam_fn(model, image_tensor, target_class=label, negative=False))
+        grad_cam_maxed = np.maximum.reduce(grad_cam_list)
+
+
+        # === Convert everything
+        original_image = denormalize_image(image_tensor)
+        heatmap = cam_to_heatmap(grad_cam_maxed)
+        overlay = overlay_image_and_heatmap(original_image, heatmap, alpha=0.5)
+
+        # # === Save original image
+        # save_path = os.path.join(CAM_DIR, f'{image_name}.jpg')
+        # original_image.save(save_path)
+
+        # === Save overlay
+        save_path = os.path.join(CAM_DIR, f'{image_name}_neggradcam_overlay_mbn.jpg')
+        if not included_classes:
+            save_path = os.path.join(CAM_DIR, f'{image_name}_gradcam_overlay_mbn.jpg')
+        overlay.save(save_path)
+        print(f"✅ neg-grad-CAM overlay img saved to: {save_path}")
--- MRP/grad_cam/scripts/generate_pseudo_masks.py
+++ MRP/grad_cam/scripts/generate_pseudo_masks.py
@@ -8,10 +8,10 @@ from torchvision import transforms
 # Add project root for config import
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from scripts.config import IMAGE_DIR, LIST_FILE, CHECKPOINT_DIR, MASK_DIR
+from scripts.config import IMAGE_DIR, TRAIN_FILE, CHECKPOINT_DIR, MASK_DIR, TRIMAP_DIR
 from utils.dataset import PetClassificationDataset
-from utils.model import get_resnet18
-from utils.cam_utils import generate_cam
+from utils.model import get_resnet18, get_mobilenet_v3_small
+from utils.grad_cam_utils import generate_grad_cam, generate_grad_cam_mobilenet
 from utils.mask_utils import cam_to_mask
 
 # Create output folder if needed
@@ -26,19 +26,69 @@ transform = transforms.Compose([
 ])
 
 # Load dataset (training only)
-dataset = PetClassificationDataset(IMAGE_DIR, LIST_FILE, transform=transform, train_only=True)
+dataset = PetClassificationDataset(IMAGE_DIR, TRAIN_FILE, transform=transform)
 
-# Load trained classifier
-model = get_resnet18(num_classes=37)
-model.load_state_dict(torch.load(os.path.join(CHECKPOINT_DIR, 'resnet18_cls.pth'), map_location='cpu'))
 
+model_name = "resnet"
+model_name = "mobilenet"
+if model_name == "resnet":
+    generate_cam_fn = generate_grad_cam
+    # Load trained classifier
+    model = get_resnet18(num_classes=37)
+    model.load_state_dict(torch.load(os.path.join(CHECKPOINT_DIR, 'resnet18_cls.pth'), map_location='cpu'))
+elif model_name == "mobilenet":
+    generate_cam_fn = generate_grad_cam_mobilenet
+    # Load trained classifier
+    model = get_mobilenet_v3_small()
+    model.load_state_dict(torch.load(os.path.join(CHECKPOINT_DIR, 'mobilenet_epoch9.pth'), map_location='cpu'))
+
+
+compute_mIoU = True
+
+mIoU = 0
+num_samples = 10
 # Generate pseudo masks
-for image_tensor, label, image_name in dataset:
-    cam = generate_cam(model, image_tensor, target_class=label)
-    mask = cam_to_mask(cam, threshold=0.25)
+for c, (image_tensor, label, image_name) in enumerate(dataset):
+    if c == num_samples:
+        break
+    excluded_classes = [i for i in range(0,37)]
+    included_classes = [0,2,7,16,20,21,37]
+    # included_classes = []
+    excluded_classes = list(set(excluded_classes) - set(included_classes))
+
+
+    # === Generate CAM
+    grad_cam_list = [
+        # negative grad-CAM for false classes
+        generate_cam_fn(model, image_tensor, target_class=c, negative=True)
+        for c in range(0, 37) if (c not in excluded_classes) and (c != label)
+    ]
+    # positive grad-CAM for the true labelled class 
+    grad_cam_list.append(generate_cam_fn(model, image_tensor, target_class=label, negative=False))
+    grad_cam_maxed = np.maximum.reduce(grad_cam_list)
+
+    mask = cam_to_mask(grad_cam_maxed, threshold=0.25)
 
     filename = os.path.splitext(image_name)[0]
     mask_img = Image.fromarray(mask, mode='L')
     mask_img.save(os.path.join(MASK_DIR, f'{filename}_mask.png'))
+
+    if compute_mIoU:
+        # Calculate mIoU
+        trimap_path = os.path.join(TRIMAP_DIR, f'{filename}.png')
+        # transform to 244x244
+        trimap = Image.open(trimap_path).convert('L')
+        trimap = transforms.Resize((224, 224))(trimap)
+        trimap = np.array(trimap)
+        # the value 1 is the foreground, extract the ground truth mask
+        gt_mask = (trimap == 1).astype(np.uint8)
+        # calculate the intersection and union
+        intersection = np.logical_and(gt_mask, mask).sum()
+        union = np.logical_or(gt_mask, mask).sum()
+        mIoU += intersection / union
+
+mIoU /= num_samples
+print(f"Mean IoU over {num_samples} samples: {mIoU}")
+
 
 print("✅ All pseudo masks generated.")
